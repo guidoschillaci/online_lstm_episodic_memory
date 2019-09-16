b80ae956473fc301c1b1c8250cbceb4ecd229a7a
Guido Schillaci <guido.schillaci@informatik.hu-berlin.de>
2019-09-16 11:49:30 +0200

    bug in pickle. Added utils file

--- scripts/utils.py
+++ scripts/utils.py
@@ -10,3 +10,6 @@ def convert_pickle_from_python3_to_python2:
         parameters = pickle.load(open(directory + 'parameters.pkl', 'rb'))
 
         pickle.dump(parameters, open(os.path.join('directory'), 'parameters.pkl'), 'wb', protocol=2)  # protcolo2 for compatibility with python2
+
+if __name__ == "__main__":
+    convert_pickle_from_python3_to_python2()
