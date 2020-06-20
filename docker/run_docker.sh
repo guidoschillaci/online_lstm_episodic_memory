edd0425c9a1ac99a243502052c13a2fafe2cdc53
Guido Schillaci <guido.schillaci@informatik.hu-berlin.de>
2020-06-20 13:34:57 +0200

    docker

---
+++ docker/run_docker.sh
@@ -0,0 +1,20 @@
+
+export DOCKER_CONTAINER_NAME=docker_container
+if [ ! "$(docker ps -q -f name=${DOCKER_CONTAINER_NAME})" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=${DOCKER_CONTAINER_NAME})" ]; then
+        echo "Cleaning up existing container named ${DOCKER_CONTAINER_NAME}"
+        # cleanup
+        docker rm $DOCKER_CONTAINER_NAME
+    fi
+    # run your container 
+    docker run -it --rm \
+      --name "$DOCKER_CONTAINER_NAME" \
+      --env="NVIDIA_VISIBLE_DEVICES=${NVIDIA_VISIBLE_DEVICES:-all}" \
+      --env="NVIDIA_DRIVER_CAPABILITIES=${NVIDIA_DRIVER_CAPABILITIES:+$NVIDIA_DRIVER_CAPABILITIES,}graphics" \
+     -p10000:10000 \
+      --runtime=nvidia \
+      --gpus all \
+      --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+      --volume="/home/ubuntu/online_lstm_episodic_memory:/online_lstm_episodic_memory/:rw"  \
+      guidoski/greenhouse:tf2-gpu-py3 bash
+fi
